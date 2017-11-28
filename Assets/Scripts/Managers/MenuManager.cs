4affb4a22ff4dc52b121032413c2405378fe9524
Unknown <gr1157@nyu.edu>
2017-11-28 16:49:16 -0500

    currentmenuwork

---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour {
+
+	public static MenuManager instance;
+
+	public int chosenGameMode = 0; //0 is DanG, 1 is PowerBall
+
+	public RectTransform[] screens;
+	public float screenLerpSpeed = 0.17f;
+
+	private int currentScreen = 0;
+
+	void Awake(){
+		instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (screens [currentScreen].anchoredPosition.x != 0) {
+			float moveAmount = Mathf.Lerp (screens [currentScreen].anchoredPosition.x, 0, screenLerpSpeed);
+			moveAmount -= screens [currentScreen].anchoredPosition.x;
+			for (int i = 0; i < screens.Length; i++) {
+				screens [i].anchoredPosition += Vector2.right * moveAmount;
+			}
+		}
+	}
+
+	public void SetCurrentScreen(int newScreen){
+		currentScreen = newScreen;
+	}
+
+	public void SetGameMode(int newGamemode){
+		chosenGameMode = newGamemode;
+	}
+
+	public void QuitGame(){
+		Debug.Log ("quitting");
+		Application.Quit ();
+	}
+}
---
+++ Assets/Scripts/Managers/PlayerReadyObject.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using InControl;
+
+public class PlayerReadyObject : MonoBehaviour {
+
+	public SubModifier dashReference;
+	public GameObject dashImage;
+	public SubModifier shieldReference;
+	public GameObject shieldImage;
+	public SubModifier teleReference;
+	public GameObject teleImage;
+	public SubModifier chosenSubmodifier;
+
+	private PlayerInput myInput;
+
+	public int playerID = 0;
+	public bool controllerAvail = false;
+	public bool lockedIn = false;
+
+	public int viewingMod {
+		get {
+			return a_viewingMod;
+		}
+		set {
+			if (value < 0) a_viewingMod = 2;
+			else if (value > 2) a_viewingMod = 0;
+			else a_viewingMod = value;
+		}
+	}
+	private int a_viewingMod = 0;
+
+	public Color notLockedColor;
+	public Color lockedColor;
+	private Image bgImage;
+
+	bool upHit = false;
+	bool downHit = false;
+	bool submitHit = false;
+	// Use this for initialization
+	void Start () {
+		chosenSubmodifier = dashReference;
+
+		bgImage = this.GetComponent<Image>();
+
+		myInput = this.GetComponent<PlayerInput>();
+
+		myInput.playerNum = playerID;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (InputManager.Devices.Count >= playerID){
+			controllerAvail = true;
+
+			upHit = InputManager.Devices[playerID - 1].DPadUp.WasPressed;
+			downHit = InputManager.Devices[playerID - 1].DPadDown.WasPressed;
+			submitHit = InputManager.Devices[playerID - 1].Action1.WasPressed;
+		}
+		else {
+			controllerAvail = false;
+		}
+
+		if (!lockedIn){ //selection thingy
+			if (bgImage.color != notLockedColor)
+				bgImage.color = notLockedColor;
+
+			if (upHit){
+				viewingMod += 1;
+			}
+			if (submitHit){
+				if (viewingMod == 0)
+					chosenSubmodifier = dashReference;
+				else if (viewingMod == 1)
+					chosenSubmodifier = shieldReference;
+				else if (viewingMod == 2)
+					chosenSubmodifier = teleReference;
+
+				lockedIn = true;
+			}
+			if (viewingMod == 0){
+				dashImage.SetActive(true);
+				shieldImage.SetActive(false);
+				teleImage.SetActive(false);
+			}
+			else if (viewingMod == 1){
+				dashImage.SetActive(false);
+				shieldImage.SetActive(true);
+				teleImage.SetActive(false);
+			}
+			else if (viewingMod == 2){
+				dashImage.SetActive(false);
+				shieldImage.SetActive(false);
+				teleImage.SetActive(true);
+			}
+
+		}
+		else {
+			if (bgImage.color != lockedColor)
+				bgImage.color = lockedColor;
+
+			if (myInput.circlePressed){
+				lockedIn = false;
+			}
+		}
+	}
+}
