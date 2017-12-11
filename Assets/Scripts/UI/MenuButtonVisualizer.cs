f86b0af8ff0218a1f575e9d0e647fafe95764a6f
Unknown <gw805@nyu.edu>
2017-12-10 22:31:01 -0500

    Sprites and Partial UI redo

---
+++ Assets/Scripts/UI/MenuButtonVisualizer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MenuButtonVisualizer : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+
+    [SerializeField] Text buttonText;
+    [SerializeField] Color normalColor;
+    [SerializeField] Color highlightColor;
+
+    // Use this for initialization
+    void Start () {
+        buttonText.color = normalColor;
+	}
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        buttonText.color = highlightColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        buttonText.color = normalColor;
+    }
+
+    public void OnPointerClick(PointerEventData eventData) // 3
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
